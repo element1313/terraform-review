1c10e05fd8c14d8020f65ed79ef05cc4e2c1c0f8
Cloud User <centos@ip-172-31-38-17.eu-west-1.compute.internal>
2020-10-18 06:54:23 +0000

    added group membership

--- class1/provider.tf
+++ class1/provider.tf
@@ -10,3 +10,13 @@ resource "aws_iam_group" "developers" {
   name = "developers"
   path = "/"
 }
+
+resource "aws_iam_group_membership" "developers_team" {
+  name = "develepers-group-membership"
+
+  users = [
+    "${aws_iam_user.tim.name}"
+  ]
+
+  group = "${aws_iam_group.developers.name}"
+}
