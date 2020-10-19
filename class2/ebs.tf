cf2bbd7960a0f91cfe0066507551095125d2f302
Cloud User <centos@ip-172-31-38-17.eu-west-1.compute.internal>
2020-10-19 05:02:38 +0000

    added volume

---
+++ class2/ebs.tf
@@ -0,0 +1,9 @@
+resource "aws_ebs_volume" "example" {
+  availability_zone = "us-east-1a"
+  size              = 40
+}
+resource "aws_volume_attachment" "ebs_att" {
+  device_name = "/dev/sdh"
+  volume_id   = "${aws_ebs_volume.example.id}"
+  instance_id = "${aws_instance.web.id}"
+}
