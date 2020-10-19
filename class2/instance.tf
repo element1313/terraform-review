a0f1240cc8e8995263b85fde5f563ae24dc3d660
Cloud User <centos@ip-172-31-38-17.eu-west-1.compute.internal>
2020-10-19 04:44:43 +0000

    added sec group

--- class2/instance.tf
+++ class2/instance.tf
@@ -1,11 +1,11 @@
 resource "aws_instance" "web" {
+    #count = 10
   ami           = "ami-0947d2ba12ee1ff75"
   instance_type = "t2.micro"
   associate_public_ip_address = "true"
   key_name = "${aws_key_pair.class.key_name}"
+  user_data =  "${file("userdata.sh")}"
+  vpc_security_group_ids = ["${aws_security_group.allow_ssh.id}"]
+
 }
 
-resource "aws_key_pair" "class" {
-  key_name   = "class-key"
-  public_key = "${file("~/.ssh/id_rsa.pub")}"
-}
