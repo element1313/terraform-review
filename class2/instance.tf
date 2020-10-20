265348272b6e7b7d08841bc0bdf646bd88319ddc
Cloud User <centos@ip-172-31-38-17.eu-west-1.compute.internal>
2020-10-20 04:19:49 +0000

    added import line

--- class2/instance.tf
+++ class2/instance.tf
@@ -8,5 +8,25 @@ resource "aws_instance" "web" {
   vpc_security_group_ids = ["${aws_security_group.allow_ssh.id}"]
   availability_zone = "us-east-1a"
 
+  tags = {
+    Name = "HelloWorld"
+    Env = "Development"
+    Client = "panera_bread"
+    Team = "DevOps"
+    Created_by = "Shokhrukh"
+  }
+
 }
 
+resource "aws_instance" "web2" {
+   ami           = "ami-0947d2ba12ee1ff75"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "HelloWorld"
+    Env = "Development"
+    Client = "panera_bread"
+    Team = "DevOps"
+    Created_by = "Shokhrukh"
+  }
+
+}
